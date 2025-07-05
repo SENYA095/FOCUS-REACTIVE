35a9289ab99667cfeca50c8e40cd792dd1415e98
SENYA095 <korolarsen404@gmail.com>
2025-07-05 20:15:51 +0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="w-full h-screen py-5 px-9 bg-black text-white ">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
