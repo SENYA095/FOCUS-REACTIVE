0a3220869fa0a5628cdd9db2eb49b89d77d0efc2
SENYA095 <korolarsen404@gmail.com>
2025-07-14 23:36:13 +0300

    added adventages scroll animation

--- src/components/Adventages.tsx
+++ src/components/Adventages.tsx
@@ -4,7 +4,7 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 const AdvantageSlide = ({ value, label, style }) => (
   <motion.div
-    className="absolute left-0 right-0 flex gap-[100px] items-start justify-center"
+    className="absolute left-0 right-0 flex gap-[100px] items-start justify-center "
     style={style}
   >
     <span className="text-[240px] w-[785px] text-right font-bold uppercase leading-[.9] tracking-[-4.8px] text-[#00E56D]">
@@ -23,15 +23,15 @@ const Advantages = () => {
 
   // Трансформації для кожного слайда
   const slide1Y = useTransform(scrollYProgress, [0, 0.3], ["0%", "-150%"]);
-  const slide2Y = useTransform(scrollYProgress, [0.3, 0.6], ["100%", "-25%"]);
-  const slide3Y = useTransform(scrollYProgress, [0.6, 1], ["100%", "0%"]);
+  const slide2Y = useTransform(scrollYProgress, [0.3, 0.6], ["210%", "-120%"]);
+  const slide3Y = useTransform(scrollYProgress, [0.6, 1], ["210%", "-90%"]);
 
   return (
-    <div ref={containerRef} className="relative h-[3000px] mt-[500px]">
-      <div className="sticky top-1/2 -translate-y-1/2">
+    <div ref={containerRef} className="relative h-[5000px] mt-[500px] ">
+      <div className="sticky top-1/2 -translate-y-1/2 ">
           <AdvantageSlide value="3.5%" label="Conversion Rate" style={{ y: slide1Y }} />
-          <AdvantageSlide value="1200$" label="Customer Lifetime Value" style={{ y: slide2Y }} />
-          <AdvantageSlide value="100" label="Website Performance" style={{ y: slide3Y }} />
+          <AdvantageSlide value="3.5%" label="Conversion Rate" style={{ y: slide2Y }} />
+          <AdvantageSlide value="3.5%" label="Conversion Rate" style={{ y: slide3Y }} />
       </div>
     </div>
   );
